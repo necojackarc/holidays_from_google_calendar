f06becc2aa48e9057906d38c2af16f37e581cf20
necojackarc <necojackarc@gmail.com>
2016-02-07 01:51:55 +0900

    Add some tools

--- spec/holidays_from_google_calendar_spec.rb
+++ spec/holidays_from_google_calendar_spec.rb
@@ -4,8 +4,4 @@ describe HolidaysFromGoogleCalendar do
   it "has a version number" do
     expect(HolidaysFromGoogleCalendar::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require "simplecov"
+require "codeclimate-test-reporter"
+
+SimpleCov.start
+CodeClimate::TestReporter.start
+
 require "holidays_from_google_calendar"
