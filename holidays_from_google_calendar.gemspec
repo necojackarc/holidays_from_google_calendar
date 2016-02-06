a2de057a9a0e5fb55a6183832f349a14c1b7a671
necojackarc <necojackarc@gmail.com>
2016-02-06 17:16:24 +0900

    Add feature retrieving holidays

--- holidays_from_google_calendar.gemspec
+++ holidays_from_google_calendar.gemspec
@@ -19,6 +19,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "google-api-client", "~> 0.9"
+  spec.add_dependency "activesupport"
+
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
--- lib/holidays_from_google_calendar.rb
+++ lib/holidays_from_google_calendar.rb
@@ -1,5 +1,62 @@
 require "holidays_from_google_calendar/version"
 
+require "google/apis/calendar_v3"
+
+require "active_support"
+require "active_support/core_ext"
+
 module HolidaysFromGoogleCalendar
-  # Your code goes here...
+  class Holidays
+    Holiday = Struct.new(:name, :date)
+
+    def initialize(nation: "usa", language: "en", api_key: nil)
+      @nation = nation
+      @language = language
+      @api_key = api_key
+    end
+
+    def in_year(date)
+      response = retrieve_from_google_calendar(
+        date_min: date.beginning_of_year,
+        date_max: date.end_of_year
+      )
+      pack_response_in_struct(response)
+    end
+
+    def in_month(date)
+      response = retrieve_from_google_calendar(
+        date_min: date.beginning_of_month,
+        date_max: date.end_of_month
+      )
+      pack_response_in_struct(response)
+    end
+
+    private
+
+    def retrieve_from_google_calendar(date_min: nil, date_max: nil)
+      service = Google::Apis::CalendarV3::CalendarService.new
+      service.key = @api_key
+      service.list_events(
+        calendar_id,
+        single_events: true,
+        order_by: "startTime",
+        time_min: date_to_time(date_min),
+        time_max: date_to_time(date_max)
+      )
+    end
+
+    def calendar_id
+      "#{@language}.#{@nation}#holiday@group.v.calendar.google.com"
+    end
+
+    def date_to_time(date)
+      Time.parse(date.iso8601).iso8601
+    end
+
+    def pack_response_in_struct(response)
+      response.items.reduce([]) do |array, item|
+        array.push(Holiday.new(item.summary, Date.parse(item.start.date)))
+      end
+    end
+  end
 end
