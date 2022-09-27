517f917fe22da9ac208f17d0f79dbf74577ec43d
necojackarc <necojackarc@gmail.com>
2022-09-27 11:37:57 +0100

    Add coverall to report test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,19 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-require "simplecov"
+require 'simplecov'
 
-SimpleCov.start
+SimpleCov.start do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+end
 
 require "rspec/its"
 require "holidays_from_google_calendar"
