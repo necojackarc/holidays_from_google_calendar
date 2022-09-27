666a42718a0862b1a8abac41243f72e3bfdf8951
necojackarc <necojackarc@gmail.com>
2022-09-27 12:10:07 +0100

    Format the file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-require 'simplecov'
+require "simplecov"
 
 SimpleCov.start do
-  if ENV['CI']
-    require 'simplecov-lcov'
+  if ENV["CI"]
+    require "simplecov-lcov"
 
     SimpleCov::Formatter::LcovFormatter.config do |c|
       c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
+      c.single_report_path = "coverage/lcov.info"
     end
 
     formatter SimpleCov::Formatter::LcovFormatter
